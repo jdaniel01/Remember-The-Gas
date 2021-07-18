8e57bb57c0ae54f037165581e16344f054f1172b
James Daniel <professor4cob@outlook.com>
2021-07-17 20:19:21 -0700

    added logo to nav and favicon

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,35 +1,54 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
+import { authenticate } from '../store/session';
 import LogoutButton from './auth/LogoutButton';
+import "./index.css"
 
 const NavBar = () => {
+  const user = useSelector(state => state.session.user)
+
   return (
     <nav>
-      <ul>
-        <li>
-          <NavLink to="/" exact={true} activeClassName="active">
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/login" exact={true} activeClassName="active">
-            Login
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/sign-up" exact={true} activeClassName="active">
-            Sign Up
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/users" exact={true} activeClassName="active">
-            Users
-          </NavLink>
-        </li>
-        <li>
-          <LogoutButton />
-        </li>
-      </ul>
+      <div>
+        <img className="rtg_logo" src="dftg-logo.png" alt="Remember The Gas Logo. The logo is an image of a red jerry can, or gas can, pouring out a green checkmark" />
+        {!user &&
+          <>
+            <div>
+              <NavLink to="/" exact={true} activeClassName="active">
+              Main
+            </NavLink>
+          </div>
+          <div>
+            <NavLink to="/about" exact={true} activeClassName="active">
+              About
+            </NavLink>
+          </div>
+          <div>
+            <NavLink to="/login" exact={true} activeClassName="active">
+              Login
+            </NavLink>
+          </div>
+          <div>
+            <NavLink to="/sign-up" exact={true} activeClassName="active">
+              Sign Up
+            </NavLink>
+            </div>
+          </>
+        }
+        {user &&
+          <>
+            <div>
+              <NavLink to="/users" exact={true} activeClassName="active">
+                Users
+              </NavLink>
+          </div>
+          <div>
+            <LogoutButton />
+            </div>
+          </>
+        }
+      </div>
     </nav>
   );
 }
