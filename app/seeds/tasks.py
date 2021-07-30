2c0b3beacab3373f2d104439d0350e43470bb350
James Daniel <professor4cob@outlook.com>
2021-07-30 14:27:21 -0700

    added task seed

--- app/seeds/tasks.py
+++ app/seeds/tasks.py
@@ -69,6 +69,6 @@ def seed_tasks():
 # SQLAlchemy doesn't have a built in function to do this
 # TRUNCATE Removes all the data from the table, and resets
 # the auto incrementing primary key
-def undo_lists():
+def undo_tasks():
     db.session.execute('TRUNCATE tasks RESTART IDENTITY CASCADE;')
     db.session.commit()
