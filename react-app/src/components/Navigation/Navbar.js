002b8b63e4bfa6bf3be9ad860de7713986854581
jdaniel01 <professor4cob@outlook.com>
2022-03-30 07:46:03 -0700

    useLocation worked, time to re-review react docs...

--- react-app/src/components/Navigation/Navbar.js
+++ react-app/src/components/Navigation/Navbar.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useLocation } from "react-router-dom";
 import { setNavStatus } from "../../store/nav";
 import RtgLogo from "./Logo";
 import Menu from "./Menu";
@@ -7,15 +8,17 @@ import "./navigation.css";
 
 const Navbar = () => {
 
+    const location = useLocation();
+    console.log(location);
 
-    return (
+    return location.pathname !== "/login" && location.pathname !== "/sign-up" ? (
         <div className="navigation_bar">
             <div className="spacer">
                 <RtgLogo />
                 <Menu />
             </div>
         </div>
-    )
+    ) : null;
 }
 
 export default Navbar;
